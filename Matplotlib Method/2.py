0606832bce1ed38f7feff6f2a720497e2bd10c75
Yugesh A <a.yugesh2@gmail.com>
2025-03-27 04:43:35 +0530

    sd

--- Matplotlib Method/2.py
+++ Matplotlib Method/2.py
@@ -1,9 +1,15 @@
 from matplotlib import pyplot as plt
-x=["Jithu","Kanna","Charu"]
+
+x=["yugesh","yugito","yugg"]
 y=[100,50,65]
+
 plt.title("Bar Graph")
+
 plt.xlabel("Name")
 plt.ylabel("Marks")
+
 plt.grid(True)
-plt.bar(x,y,color="magenta")
+
+plt.bar(x,y,color="blue")
+
 plt.show()
---
+++ test.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x = ['yuge','yuggito','adslldsjkj']
+
+y = [23,56,90]
+
+plt.xlabel("name")
+plt.ylabel("Mark")
+
+plt.title("namemark")
+
+plt.bar(x,y, color="blue")
+plt.show()
