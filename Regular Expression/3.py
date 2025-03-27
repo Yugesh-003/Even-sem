f7ef7741a2aaccf73079f8767322529c42ca790f
Yugesh A <a.yugesh2@gmail.com>
2025-03-27 10:00:26 +0530

    ii

--- Regular Expression/3.py
+++ Regular Expression/3.py
@@ -5,3 +5,8 @@ def remove(ip):
     print("Updated IP:", result)
 
 remove("192.168.001.001")
+
+
+#pattern = "^[a-zA-Z0-9]*$"  re.search
+#pattern = "^8"              re.match
+#result = re.sub(r'\b0+(\d)', r'\1', ip)
