939855f598959b54d74c7b1b7ea5c02487139fe3
Yugesh A <a.yugesh2@gmail.com>
2025-03-27 10:01:58 +0530

    ww

--- Oops Concepts/overriding.py
+++
@@ -1,8 +0,0 @@
-class Marks:
-    def calculate(self):
-        print("Calculating marks...")
-class Student(Marks):
-    def calculate(self):
-        print("Student's Marks Calculated")
-s = Student()
-s.calculate()
